aedb3857f19be6d3d595929bd1566d75472de289
zanehankin <zanehankin@yahoo.com>
2019-06-04 13:16:51 -0700

    Apply euler angle to arrow

--- Detect-Objects-In-Path/ARSceneManager.swift
+++ Detect-Objects-In-Path/ARSceneManager.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Zane Hankin on 5/20/19.
 //  Copyright © 2019 Zane Hankin. All rights reserved.
-//
 
 import Foundation
 import ARKit
@@ -15,11 +14,11 @@ class ARSceneManager: NSObject{
     private var planes = [UUID: Plane]()
     var existingVerticalPlane: ARHitTestResult.ResultType = []
     
+    var ship: SCNNode? = nil
+    
     func attach(to sceneView: ARSCNView){
         self.sceneView = sceneView
-        
         self.sceneView!.delegate = self
-        
         configureSceneView(self.sceneView!)
     }
     
@@ -40,33 +39,44 @@ class ARSceneManager: NSObject{
 extension ARSceneManager: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
-        print("Found plane: \(planeAnchor)")
-        
-        let plane = Plane(anchor: planeAnchor)
-        planes[anchor.identifier] = plane
-        node.addChildNode(plane)
-        
-        print(planeAnchor.geometry.boundaryVertices)
-        let point1 = planeAnchor.geometry.boundaryVertices[0]
-        let point2 = planeAnchor.geometry.boundaryVertices[1]
-        g
-        let nx1 = point1.x
-        let nz1 = point1.z
-        
-        let nx2 = point2.x
-        let nz2 = point2.z
+        DispatchQueue.main.async {
+            
+            let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
+            self.ship = shipScene.rootNode.childNodes.first
+            node.addChildNode(self.ship!)
+            
+            self.ship?.position = SCNVector3(x: 0, y: 0, z: -30)
+            
+            self.sceneView?.pointOfView?.addChildNode(self.ship!)
+            
+            
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+            
+            print("Found plane: \(planeAnchor)")
+            
+            let plane = Plane(anchor: planeAnchor)
+            self.planes[anchor.identifier] = plane
+            node.addChildNode(plane)
+            
+            print(planeAnchor.geometry.boundaryVertices)
+            let point1 = planeAnchor.geometry.boundaryVertices[0]
+            let point2 = planeAnchor.geometry.boundaryVertices[1]
+            
+            let nx1 = point1.x
+            let nz1 = point1.z
+            
+            let nx2 = point2.x
+            let nz2 = point2.z
+            
+            var ang = atan2(nz2-nz1, nx2-nx1)
+            ang = ang * 180 / .pi
+            print(ang)
+            
+            self.ship?.eulerAngles.z = ang
 
-        var ang = atan2(nz2-nz1, nx2-nx1)
-        ang = ang * 180 / .pi
-        print(ang)
-        
-        
-        
-
-        //Use an arrow to be positioned at the angle of the viewer and then to turn until the angles = one another
-}
+            //Use an arrow to be positioned at the angle of the viewer and then to turn until the angles = one another
+        }
+    }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
@@ -75,20 +85,17 @@ extension ARSceneManager: ARSCNViewDelegate {
             plane.updateWith(anchor: planeAnchor)
         }
         
-        
-        
-//        if let plane = SCNPlane(width: CGFloat(ARPlaneAnchor.extent.x), height: CGFloat(ARPlaneAnchor.extent.z)){
-////            anchor.extent.z < 100
-//            print("Turn!")
-//        }
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        
         planes.removeValue(forKey: anchor.identifier)
+        
     }
     
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+    }
 }
 
 //        determine angle of rotation from Swift in ARKit
-//        var existingPlane: ARHitTestResult.ResultType { get }
-//        if let results = sceneView.hitTest(ARPlaneAnchor, [.existingVerticalPlaneUsingGeometry]){
